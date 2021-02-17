22b3869e1a1c2ea63042abefe2368ad96ff6b6cd
koienshi <55773406+koienshi@users.noreply.github.com>
2021-02-17 02:32:22 -0500

    formatting

--- scripts/script.js
+++ scripts/script.js
@@ -22,53 +22,53 @@ const lessonSection = document.getElementById("lesson"); //section where the les
 
 const elements = {
     image: function() {
-      const img = document.createElement("img");
-      img.src = this.content.link;
-      if(typeof this.content.class == "string") {
-        img.classList.add(this.content.class);
-      }
-      if(typeof this.content.title == "string") {
-         img.id = this.content.title; //set
-         createDirectory(this.content.title); //add title name of each section to the directory
-      }
-      lessonSection.appendChild(img);
+		const img = document.createElement("img");
+		img.src = this.content.link;
+		if(typeof this.content.class == "string") {
+			img.classList.add(this.content.class);
+		}
+		if(typeof this.content.title == "string") {
+			img.id = this.content.title; //set
+			createDirectory(this.content.title); //add title name of each section to the directory
+		}
+		lessonSection.appendChild(img);
     },
     hyperlink: function() {
-      const link = document.createElement("a");
-      link.href = this.content.link;
-      link.textContent = this.content.text;
-      if(typeof this.content.class == "string") {
-        link.classList.add(this.content.class);
-      }
-      lessonSection.appendChild(link);
+		const link = document.createElement("a");
+		link.href = this.content.link;
+		link.textContent = this.content.text;
+		if(typeof this.content.class == "string") {
+			link.classList.add(this.content.class);
+		}
+		lessonSection.appendChild(link);
     },
     paragraph: function() {
-      const p = document.createElement("p");
-      p.innerHTML = this.content;
-      lessonSection.appendChild(p);
+		const p = document.createElement("p");
+		p.innerHTML = this.content;
+		lessonSection.appendChild(p);
     },
     header: function() {
-      const head = document.createElement("h4");
-      head.innerHTML = this.content;
-      lessonSection.appendChild(head);
+		const head = document.createElement("h4");
+		head.innerHTML = this.content;
+		lessonSection.appendChild(head);
     },
     button: function() {
-      const button = document.createElement("button");
-      button.setAttribute('onclick', "window.open('" + this.content.link + "','_blank');");
-      button.textContent = this.content.text;
-      lessonSection.appendChild(button);
+		const button = document.createElement("button");
+		button.setAttribute('onclick', "window.open('" + this.content.link + "','_blank');");
+		button.textContent = this.content.text;
+		lessonSection.appendChild(button);
     },
     video: function() {
-      const video = document.createElement("iframe");
-      const yt = document.createElement("div");
-      yt.classList.add("yt-container");
-      video.classList.add("responsive-iframe");
-      video.setAttribute('frameborder', "0");
-      video.setAttribute('allowfullscreen', "");
-      video.setAttribute('allow','accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture');
-      video.src = this.content.link;
-      yt.appendChild(video);
-      lessonSection.appendChild(yt);
+		const video = document.createElement("iframe");
+		const yt = document.createElement("div");
+		yt.classList.add("yt-container");
+		video.classList.add("responsive-iframe");
+		video.setAttribute('frameborder', "0");
+		video.setAttribute('allowfullscreen', "");
+		video.setAttribute('allow','accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture');
+		video.src = this.content.link;
+		yt.appendChild(video);
+		lessonSection.appendChild(yt);
     }
 };
 // <iframe width="560" height="315" src="https://www.youtube.com/embed/U8r3oTVMtQ0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
@@ -112,16 +112,16 @@ const elements = {
 const lessons = db.collection("lessons");
 
 function loadLesson(lesson) {
-   //how many sections the edition has, iterates through sections (below)
+   	//how many sections the edition has, iterates through sections (below)
       lessons.doc("biology").collection("gel-electro").doc("background").get()
         .then(doc => {
             const data = doc.data(); //retrieves all the sections as 'objects'
             console.log(data);
             let entries1 = Object.entries(data); //return array of each object's key-value pairs
             for (const [key, value] of entries1.sort()) { //loop through each element (key-value) in the array
-              let keyword = `${key}`; //the different key properties
-              let values = `${value}`; //the different value properties
-              lessonSection.append(values); 
+				let keyword = `${key}`; //the different key properties
+				let values = `${value}`; //the different value properties
+				lessonSection.append(values); 
             }
         });
   }
@@ -129,51 +129,51 @@ function loadLesson(lesson) {
 //FIREBASE REALTIME DATABASE FUNCTIONS
 
 
-  function writeUserData(first_name, last_name, email, password, birthday, imageUrl, realtime_db) {
-    //using email as tree structure for users because its a very unique identifier
-    //realtime_db = firebase.database();
-    realtime_db.ref('users/' + first_name + "-" + last_name).set({
-      first_name: first_name,
-      last_name: last_name,
-      email: email,
-      password: password,
-      birthday: birthday,
-      profile_picture : imageUrl
-    });
-  }
+function writeUserData(first_name, last_name, email, password, birthday, imageUrl, realtime_db) {
+//using email as tree structure for users because its a very unique identifier
+//realtime_db = firebase.database();
+	realtime_db.ref('users/' + first_name + "-" + last_name).set({
+		first_name: first_name,
+		last_name: last_name,
+		email: email,
+		password: password,
+		birthday: birthday,
+		profile_picture : imageUrl
+	});
+}
 
-  //AN ATTEMPT!
-  //Login/Signup Required Fields
-  const fname = document.getElementByID('fname');
-  const lname = document.getElementByID('lname');
-  const emailSignUp = document.getElementByID('emailSignUp');
-  const passwordSignUp = document.getElementByID('passwordSignUp');
-  const passwordConfSignUp = document.getElementByID('passwordConfSignUp');
-  const form = document.getElementByID('form');
-  const errorElement = document.getElementById('error')
+//AN ATTEMPT!
+//Login/Signup Required Fields
+const fname = document.getElementByID('fname');
+const lname = document.getElementByID('lname');
+const emailSignUp = document.getElementByID('emailSignUp');
+const passwordSignUp = document.getElementByID('passwordSignUp');
+const passwordConfSignUp = document.getElementByID('passwordConfSignUp');
+const form = document.getElementByID('form');
+const errorElement = document.getElementById('error')
 
-  form.addEventListener('submit', (e) => {
-    let messages = []
-    if (fname.value == '' || fname.value == null){
-      messages.push('First Name is required')
-    }
+form.addEventListener('submit', (e) => {
+	let messages = []
+	if (fname.value == '' || fname.value == null){
+		messages.push('First Name is required')
+	}
 
-    if (lname.value == '' || lname.value == null){
-      messages.push('Last Name is required')
-    }
-    if (emailSignUp.value == '' || emailSignUp.value == null){
-      messages.push('Email is required')
-    }
+	if (lname.value == '' || lname.value == null){
+		messages.push('Last Name is required')
+	}
+	if (emailSignUp.value == '' || emailSignUp.value == null){
+		messages.push('Email is required')
+	}
 
-    if (passwordSignUp.value.length <= 6){
-      messages.push('Password must be longer than 6 characters')
-    }
+	if (passwordSignUp.value.length <= 6){
+		messages.push('Password must be longer than 6 characters')
+	}
 
-    if (messages.length > 0){
-      e.preventDefault()
-      errorElement.innerText = messages.join(', ')
-    }
-  })
+	if (messages.length > 0){
+		e.preventDefault()
+		errorElement.innerText = messages.join(', ')
+	}
+})
 
 
 
